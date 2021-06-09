558141ebae0753139a94be9353bc01e70215ef8d
ajiinisti <60378691+ajiinisti@users.noreply.github.com>
2021-06-09 18:57:44 +0700

    Implement ML in Cloud Environtment

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, request
 from flask_cors import CORS
 from model import predict
 import json
+import tensorflow as tf
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+session_config = tf.compat.v1.ConfigProto(
+      inter_op_parallelism_threads=5,
+      intra_op_parallelism_threads=5,
+      allow_soft_placement=True)
 
 UPLOAD_FOLDER = '/upload'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -23,11 +28,12 @@ def input_id(id):
 
 @app.route('/predict', methods=['GET','POST'])
 def predict_images():
-
     data = request.files.get("file")
     if data == None:
         return 'Got Nothing'
     else:
         prediction = predict(data)
-
     return json.dumps(prediction)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
